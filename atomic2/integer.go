8cee451facd0a2200adccbe99fa4e22d053f305f
Blade Pan <blade@msn.cn>
2014-04-28 18:13:20 -0400

    name change

--- atomic2/integer.go
+++ atomic2/integer.go
@@ -1,25 +1,25 @@
-package atomic
+package atomic2
 
 import (
 	"sync/atomic"
 )
 
-type atomicInt32 int32
+type AtomicInt32 int32
 
-func NewAtomicInt32(init int32) *atomicInt32 {
-	i := atomicInt32(init)
+func NewAtomicInt32(init int32) *AtomicInt32 {
+	i := AtomicInt32(init)
 	return &i
 }
 
 //Atomically increments by one the current value.
 //Returns: the updated value
-func (i *atomicInt32) IncrementAndGet() int32 {
+func (i *AtomicInt32) IncrementAndGet() int32 {
 	return i.AddAndGet(1)
 }
 
 //Atomically increments by delta the current value.
 //Returns: the updated value
-func (i *atomicInt32) AddAndGet(delta int32) int32 {
+func (i *AtomicInt32) AddAndGet(delta int32) int32 {
 	address := (*int32)(i)
 	swapped := false
 	var oldValue int32
@@ -34,13 +34,13 @@ func (i *atomicInt32) AddAndGet(delta int32) int32 {
 
 //Atomically sets the value to update if the current value equals to expect
 //Returns: true if successful
-func (i *atomicInt32) CompareAndSwap(expect int32, update int32) (swapped bool) {
+func (i *AtomicInt32) CompareAndSwap(expect int32, update int32) (swapped bool) {
 	address := (*int32)(i)
 	swapped = atomic.CompareAndSwapInt32(address, expect, update)
 	return
 }
 
-func (c *atomicInt32) Get() int32 {
+func (c *AtomicInt32) Get() int32 {
 	address := (*int32)(c)
 	return atomic.LoadInt32(address)
 }
